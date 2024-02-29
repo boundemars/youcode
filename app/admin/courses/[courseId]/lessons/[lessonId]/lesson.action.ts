39de460789e19831ed287f4bf91b2426f7bb4def
youssef serbouti <yserbouti@gmail.com>
2024-02-29 16:36:49 +0100

    #16 - Add markdown editor to lesson

--- app/admin/courses/[courseId]/lessons/[lessonId]/lesson.action.ts
+++ app/admin/courses/[courseId]/lessons/[lessonId]/lesson.action.ts
@@ -26,3 +26,32 @@ export const lessonActionEditDetails = authenticatedAction(
     return { message: "Lesson updated successfully", lesson };
   }
 );
+
+const LessonActionEditContentSchema = z.object({
+  lessonId: z.string(),
+  markdown: z.string(),
+});
+
+export const lessonActionEditContent = authenticatedAction(
+  LessonActionEditContentSchema,
+  async ({ lessonId, markdown }, { userId }) => {
+    const lesson = await prisma.lesson.update({
+      where: {
+        id: lessonId,
+        course: {
+          creatorId: userId,
+        },
+      },
+      data: {
+        content: markdown,
+      },
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    return {
+      message: "Lesson updated successfully",
+      lesson,
+    };
+  }
+);
--- app/admin/courses/[courseId]/lessons/[lessonId]/page.tsx
+++ app/admin/courses/[courseId]/lessons/[lessonId]/page.tsx
@@ -10,10 +10,11 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getRequiredAuthSession } from "@/lib/auth";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { MdxEditor } from "./content/MdxEditor";
 import { LessonDetail } from "./form/LessonDetailsForm";
 import { getAdminLesson } from "./lesson.query";
 
-export default async function LessonPage({
+export default async function CourseLessonsPage({
   params,
 }: {
   params: {
@@ -29,7 +30,7 @@ export default async function LessonPage({
   }
 
   return (
-    <Layout>
+    <Layout className="max-w-5xl">
       <LayoutHeader>
         <LayoutTitle>{lesson.name}</LayoutTitle>
       </LayoutHeader>
@@ -44,13 +45,21 @@ export default async function LessonPage({
           Back
         </Link>
       </LayoutActions>
-      <LayoutContent>
-        <Card className="flex-[2]">
+      <LayoutContent className="flex flex-col gap-4 lg:flex-row">
+        <Card className="flex-1">
           <CardHeader>
             <CardTitle>Details</CardTitle>
           </CardHeader>
           <CardContent className="flex flex-col gap-2">
             <LessonDetail defaultValue={lesson} />
+          </CardContent>
+        </Card>
+        <Card className="flex-[3]">
+          <CardHeader>
+            <CardTitle>Content</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-2">
+            <MdxEditor lessonId={lesson.id} markdown={lesson.content} />
           </CardContent>
         </Card>
       </LayoutContent>
